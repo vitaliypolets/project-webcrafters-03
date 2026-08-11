403fbf320da9c330f1ae3f9576829e6c32b99251
TimothyY <timjurev@gmail.com>
2026-08-11 23:56:29 +0300

    feat(home): update users service and add front-end starting  home components

--- frontend/src/features/home/home.types.ts
+++ frontend/src/features/home/home.types.ts
@@ -1,2 +1,20 @@
 // TODO: реалізувати відповідно до docs/OWNERSHIP_MAP.md
-export {};
+export interface Creator {
+  id: string;
+  _id: string;
+  name: string;
+  avatarUrl: string | null;
+  articlesCount: number;
+}
+
+export interface CreatorsResponse {
+  data: {
+    users: Creator[];
+    page: number;
+    perPage: number;
+    totalItems: number;
+    totalPages: number;
+    hasNextPage: boolean;
+  };
+  message: string;
+}
