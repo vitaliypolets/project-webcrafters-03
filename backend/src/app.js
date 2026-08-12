9f6f9d86c0b9715396f10119bb1dcd08762ee95d
vitaliypolets <vertinano@gmail.com>
2026-08-12 14:57:54 +0300

    Chore/project setup (#126)
    * edit routes
    
    * fix: align frontend articlesAmount field
    
    * fix: component MyArticles
    
    * fix: component MyArticles
    
    * fix: allow Cloudinary HTTP images
    
    * feat: add Swagger API documentation

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,41 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import { pinoHttp } from 'pino-http';
+import swaggerUi from 'swagger-ui-express';
+
 import { env } from './config/env.js';
+import { swaggerSpec } from './config/swagger.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFound } from './middlewares/notFound.js';
 import { apiRouter } from './routes/index.js';
 
 export const app = express();
+
 app.use(pinoHttp());
-app.use(cors({ origin: env.frontendUrl, credentials: true }));
+
+app.use(
+  cors({
+    origin: env.frontendUrl,
+    credentials: true,
+  }),
+);
+
 app.use(express.json({ limit: '1mb' }));
 app.use(cookieParser());
+
+//API ROUTES
+
 app.use('/api', apiRouter);
+
+//SWAGGER
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get('/api-docs.json', (req, res) => {
+  res.status(200).json(swaggerSpec);
+});
+
+//ERROR HANDLERS
+
 app.use(notFound);
 app.use(errorHandler);
