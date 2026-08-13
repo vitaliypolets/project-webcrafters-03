39cf8c65247745e7873b418951cb4d0a59f511be
TetianaMendel <tanyabulasvska@ukr.net>
2026-08-13 22:51:30 +0300

    fix: fix article creation flow and auto-generate description

--- backend/src/modules/articles/create/article-create.controller.js
+++ backend/src/modules/articles/create/article-create.controller.js
@@ -17,8 +17,5 @@ export const createArticleController = async (req, res) => {
 
   res.status(201).json(article);
 
-console.log('BODY:', req.body);
-console.log('FILE:', req.file?.originalname);
-console.log('USER:', req.user?._id);
 };
 
