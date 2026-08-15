601c4ba05e601cd34b01e1fb8cb3a5b937731d88
Vitaliy Polets <vertinano@gmail.com>
2026-08-15 23:49:56 +0300

    feat: add canonical ArticleDetails type

--- frontend/src/types/article.ts
+++ frontend/src/types/article.ts
@@ -17,8 +17,18 @@ export type Article = {
   id: string;
   title: string;
   description: string;
-  imageUrl: string;
+  imageUrl: string | null;
   publicationDate: string;
   author: PublicUser;
   isBookmarked?: boolean;
+};
+
+export type ArticleDetails = {
+  id: string;
+  title: string;
+  description: string;
+  article: string;
+  imageUrl: string | null;
+  publicationDate: string;
+  viewsCount: number;
 };
