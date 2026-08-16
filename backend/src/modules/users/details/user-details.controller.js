ba3cf0e12c046437b40b087014deef3d3be54e66
Анастасія Кошуцька <zaporozhetsnastya89@gmail.com>
2026-08-16 15:33:11 +0300

    add message for controller

--- backend/src/modules/users/details/user-details.controller.js
+++ backend/src/modules/users/details/user-details.controller.js
@@ -1,5 +1,5 @@
 // TODO (учасник №7): controllers
-import { getUserDetails } from './user-details.service.js';
+import { getUserDetails } from "./user-details.service.js";
 
 export const getUserDetailsController = async (req, res) => {
   const { userId } = req.params;
@@ -8,5 +8,6 @@ export const getUserDetailsController = async (req, res) => {
 
   res.status(200).json({
     data: user,
+    message: "Success",
   });
 };
