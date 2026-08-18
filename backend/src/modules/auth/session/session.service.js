189fba32b339229b8636cfdcda6b584b8516ecdc
Viktor Tar <tarviky@gmail.com>
2026-08-18 09:30:26 +0300

    !mongoose.isValidObjectId added

--- backend/src/modules/auth/session/session.service.js
+++ backend/src/modules/auth/session/session.service.js
@@ -1,36 +1,41 @@
-import bcrypt from 'bcrypt';
-import { Session } from '../../../models/Session.js';
-import { User } from '../../../models/User.js';
-import { HttpError } from '../../../utils/HttpError.js';
-import { createAuthSession } from '../shared/authSession.js';
-import { createAccessToken, createRefreshToken } from '../shared/authTokens.js';
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import { Session } from "../../../models/Session.js";
+import { User } from "../../../models/User.js";
+import { HttpError } from "../../../utils/HttpError.js";
+import { createAuthSession } from "../shared/authSession.js";
+import { createAccessToken, createRefreshToken } from "../shared/authTokens.js";
 
 export const refreshAuthSession = async (refreshToken, sessionId) => {
   if (!refreshToken) {
-    throw new HttpError(401, 'Refresh token is missing');
+    throw new HttpError(401, "Refresh token is missing");
   }
 
   let session = null;
 
   if (sessionId) {
-    session = await Session.findById(sessionId).select('+refreshTokenHash');
+    if (!mongoose.isValidObjectId(sessionId)) {
+      throw new HttpError(401, "Session not found or expired");
+    }
+
+    session = await Session.findById(sessionId).select("+refreshTokenHash");
   }
 
   if (!session) {
-    throw new HttpError(401, 'Session not found or expired');
+    throw new HttpError(401, "Session not found or expired");
   }
 
   const isTokenValid = await bcrypt.compare(refreshToken, session.refreshTokenHash);
 
   if (!isTokenValid) {
     await Session.deleteOne({ _id: session._id });
-    throw new HttpError(401, 'Invalid refresh token');
+    throw new HttpError(401, "Invalid refresh token");
   }
 
   const user = await User.findById(session.userId);
 
   if (!user) {
-    throw new HttpError(404, 'User not found');
+    throw new HttpError(404, "User not found");
   }
 
   await Session.deleteOne({ _id: session._id });
